c9444a228bfc58a636012f07d1089d438ef1097b
Siddharth N <s.notani@lancaster.ac.uk>
2018-05-22 06:14:22 +0100

    Added comments

--- pacman.c
+++ pacman.c
@@ -3,10 +3,10 @@
 #include <time.h>
 #include <stdio.h>
 
-#define ROWS 25
-#define COLUMNS 29
+#define ROWS 25 //rows for the maze
+#define COLUMNS 29 //columns ffor the maze
 
-char maze[ROWS][COLUMNS] = {
+char maze[ROWS][COLUMNS] = { //the maze for pacman
 {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
 {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#','.','.','.','.','.','.','.','.','.','.','.','.','#'},
 {'#','.','#','#','#','#','.','#','#','#','#','#','.','#','#','.','#','#','#','#','#','.','#','#','#','#','.','#'},
@@ -32,8 +32,6 @@ char maze[ROWS][COLUMNS] = {
 {'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#'},
 {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
 };
-
-
 
 /*void ghostmover(int ghosty, int ghostx)
 {	
@@ -80,45 +78,45 @@ char maze[ROWS][COLUMNS] = {
 
 int main()
 {	
-	WINDOW *scoreboard;	
-	WINDOW *title;
-	int pacmany = 14, pacmanx = 13;
-	int ghosty = 10, ghostx = 13; 
-	int height = 7, width = 20;
-	int scoreboardy = 3, scoreboardx = 40;
-	int titley = 1, titlex = 40;
-	int score = 0, level = 1, lives = 3;
-	int key;
+	WINDOW *scoreboard;	//window to display scoreboard
+	WINDOW *title;	//window to display the title of the scoreboard
+	int pacmany = 14, pacmanx = 13; //defines the initial position of the pacman
+	int ghosty = 10, ghostx = 13;  //defines the initial position of the ghost
+	int height = 7, width = 20;	//height and width for scoreboard window
+	int scoreboardy = 3, scoreboardx = 40; //x and y coordinates for the location of the scoreboard
+	int titley = 1, titlex = 40; //title located on top of the scoreboard
+	int score = 0, level = 1, lives = 3; //sets the score, level and lives in the scoreboard
+	int key; //key presses
 	
-	initscr();			/* Start curses mode 		  */
-	title = newwin(height, width, titley, titlex);
-	box(title, 0, 0);
-	scoreboard = newwin(height, width, scoreboardy, scoreboardx);
-	box(scoreboard, 0, 0);
-	curs_set(FALSE);	// dont show cursor
-	keypad(stdscr, TRUE);
-	refresh();			/* Print it on to the real screen */
-	//getch();			/* Wait for user input */
-	for(int i=0; i<ROWS; i++){
+	initscr();			// Start curses mode 	
+	title = newwin(height, width, titley, titlex); //opens window for the scoreboard title
+	box(title, 0, 0);  	//sets the title at the start of the window title
+	scoreboard = newwin(height, width, scoreboardy, scoreboardx); //opens window for the scoreboard 
+	box(scoreboard, 0, 0); //sets the title at the start of the window scoreboard
+	curs_set(FALSE);	//dont show cursor
+	keypad(stdscr, TRUE); //allows to use the arrow keys
+	refresh();			//print it on to the real screen 
+
+	for(int i=0; i<ROWS; i++){ 	//nested loops for 2d maze
 		for(int j=0; j<COLUMNS; j++){
-			printw("%c", maze[i][j]);
+			printw("%c", maze[i][j]); //print the maze
 		}
-		printw("\n");
+		printw("\n"); 
 	}
 	
-	move(pacmany,pacmanx);
-	refresh();
+	move(pacmany,pacmanx); //moves the cursos to the pacman's initial position
+	refresh();	//refresh and print on the screen
 	
-	while((key = getch()) != 'q')
+	while((key = getch()) != 'q') //key 'q' to quit the program
 	{
 		
-		if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN){
-			mvwprintw(title, 1, 1, "SCOREBOARD");
-			mvwprintw(scoreboard, 1, 1, "Level = %d", level);
-			mvwprintw(scoreboard, 2, 1, "Lives = %d", lives);
-			mvwprintw(scoreboard, 3, 1, "Score = %d", score);
+		if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN){ //checks if any key es pressed (game starts)
+			mvwprintw(title, 1, 1, "SCOREBOARD");	//prints the title of the scoreboard
+			mvwprintw(scoreboard, 1, 1, "Level = %d", level); //shows the levels of the game in the scoreboard
+			mvwprintw(scoreboard, 2, 1, "Lives = %d", lives); //shows the lives remaining in the scoreboard
+			mvwprintw(scoreboard, 3, 1, "Score = %d", score); //shows the score of the game in the scoreboard
 			wrefresh(title);
-			wrefresh(scoreboard);
+			wrefresh(scoreboard);		//refresh the window and print on screen
 		
 			//ghostmover(ghosty, ghostx);
 		}
@@ -126,10 +124,10 @@ int main()
 		switch(key)
 		{
 			case KEY_LEFT:
-					
-					if(maze[pacmany][pacmanx-1] != '#')
+														 
+					if(maze[pacmany][pacmanx-1] != '#') //checks for the wall in the left direction
 					{
-						if(maze[pacmany][pacmanx-1] == '.')
+						if(maze[pacmany][pacmanx-1] == '.') 
 						{
 							score = score + 10; //add 10 points each time it collects a pill
 						}
@@ -139,19 +137,19 @@ int main()
 							score = score + 50; //adds 50 points for the power pill
 						}
 						
-						if(maze[pacmany][pacmanx-1] == 'M')
+						if(maze[pacmany][pacmanx-1] == 'M') //check the collision for pacman and the ghost and move pacman to initial position
 						{
-							lives--;
+							lives--;		//removes a life if collision with ghost occurs
 							maze[pacmany][pacmanx] = ' ';
 							mvaddch(pacmany,pacmanx,' ');
 							pacmany = 14;
 							pacmanx = 13;
-							mvaddch(pacmany, pacmanx, '>');
+							mvaddch(pacmany, pacmanx, '>'); //set pacman to initial place
 							move(pacmany, pacmanx);
 							refresh();
 						}
 						
-						maze[pacmany][pacmanx] = ' ';
+						maze[pacmany][pacmanx] = ' ';	//if there is no wall, it keeps on moving left
 						mvaddch(pacmany,pacmanx,' ');
 						pacmanx--;
 						mvaddch(pacmany, pacmanx, '>');
@@ -162,7 +160,7 @@ int main()
 			
 			case KEY_RIGHT:
 					
-					if(maze[pacmany][pacmanx+1] != '#')
+					if(maze[pacmany][pacmanx+1] != '#') //checks for the wall in the right direction
 					{
 						if(maze[pacmany][pacmanx+1] == '.')
 						{
@@ -176,17 +174,17 @@ int main()
 						
 						if(maze[pacmany][pacmanx+1] == 'M')
 						{
-							lives--;
+							lives--;			//removes a life if collision with ghost occurs
 							maze[pacmany][pacmanx] = ' ';
 							mvaddch(pacmany,pacmanx,' ');
 							pacmany = 14;
 							pacmanx = 13;
-							mvaddch(pacmany, pacmanx, '>');
+							mvaddch(pacmany, pacmanx, '>'); //set pacman to initial place
 							move(pacmany, pacmanx);
 							refresh();
 						}
 						
-						maze[pacmany][pacmanx] = ' ';
+						maze[pacmany][pacmanx] = ' ';	//if there is no wall, it keeps on moving right
 						mvaddch(pacmany,pacmanx,' ');
 						pacmanx++;
 						mvaddch(pacmany, pacmanx, '<');
@@ -197,7 +195,7 @@ int main()
 			
 			case KEY_UP:
 					
-					if(maze[pacmany-1][pacmanx] != '#')
+					if(maze[pacmany-1][pacmanx] != '#') //checks for the wall in the upwards direction
 					{
 						if(maze[pacmany-1][pacmanx] == '.')
 						{
@@ -211,17 +209,17 @@ int main()
 						
 						if(maze[pacmany-1][pacmanx] == 'M')
 						{
-							lives--;
+							lives--;			//removes a life if collision with ghost occurs
 							maze[pacmany][pacmanx] = ' ';
 							mvaddch(pacmany,pacmanx,' ');
 							pacmany = 14;
 							pacmanx = 13;
-							mvaddch(pacmany, pacmanx, '>');
+							mvaddch(pacmany, pacmanx, '>'); //set pacman to initial place
 							move(pacmany, pacmanx);
 							refresh();
 						}
 						
-						maze[pacmany][pacmanx] = ' ';
+						maze[pacmany][pacmanx] = ' ';	////if there is no wall, it keeps on moving up
 						mvaddch(pacmany,pacmanx,' ');
 						pacmany--;
 						mvaddch(pacmany, pacmanx, 'V');
@@ -232,7 +230,7 @@ int main()
 					
 			case KEY_DOWN:
 					
-					if(maze[pacmany+1][pacmanx] != '#' && maze[pacmany+1][pacmanx] != '-')
+					if(maze[pacmany+1][pacmanx] != '#' && maze[pacmany+1][pacmanx] != '-') //checks for the wall and ghost box in the downwards direction
 					{
 						if(maze[pacmany+1][pacmanx] == '.')
 						{
@@ -246,17 +244,17 @@ int main()
 						
 						if(maze[pacmany+1][pacmanx] == 'M')
 						{
-							lives--;
+							lives--;			//removes a life if collision with ghost occurs
 							maze[pacmany][pacmanx] = ' ';
 							mvaddch(pacmany,pacmanx,' ');
 							pacmany = 14;
 							pacmanx = 13;
-							mvaddch(pacmany, pacmanx, '>');
+							mvaddch(pacmany, pacmanx, '>'); //set pacman to initial place
 							move(pacmany, pacmanx);
 							refresh();
 						}
 						
-						maze[pacmany][pacmanx] = ' ';
+						maze[pacmany][pacmanx] = ' ';  //if there is no wall, it keeps on moving down
 						mvaddch(pacmany,pacmanx,' ');
 						pacmany++;
 						mvaddch(pacmany, pacmanx, ACS_UARROW);
@@ -267,8 +265,8 @@ int main()
 					
 		}
 		
-		if(score == 2300){
-			level++;
+		if(score == 2300){  //if all the pills are eaten, game is won
+			level++;		//goes to next level
 			mvwprintw(scoreboard, 4, 1, "Congratulations!");
 			mvwprintw(scoreboard, 5, 1, "Next level: %d", level);
 			wrefresh(scoreboard);
@@ -276,7 +274,7 @@ int main()
 			main();
 		}
 		
-		if(lives < 1){
+		if(lives < 1){		//if lives are over, game over
 			mvwprintw(scoreboard, 6, 1, "GAME OVER!");
 			mvwprintw(scoreboard, 7, 1, "No lives left!");
 			wrefresh(scoreboard);
@@ -284,7 +282,7 @@ int main()
 		
 	}
 	
-	endwin();			/* End curses mode		  */
+	endwin();			// End curses mode		  
 
 	return 0;
 }
